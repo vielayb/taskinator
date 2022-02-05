ce4598573d7d04a80b1a9f215c8d1a7537a76c35
Vielay Bangphaxay <pounds@Vielays-MacBook-Pro.local>
2022-02-05 07:29:57 -0500

    submitting form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+  event.preventDefault();
+
     var listItemEl = document.createElement("li");
+    // adding css styling to the element
     listItemEl.className = "task-item";
+    // adding text to the new list element
     listItemEl.textContent = "This is a new task.";
+    // pushing the new listItem to the tasksToDoEl which is the <ul>
     tasksToDoEl.appendChild(listItemEl);
   }
 
-  buttonEl.addEventListener("click", createTaskHandler);
+  formEl.addEventListener("submit", createTaskHandler);
